c268cc57cef315db1cdd1ed732fe1012b95c82cb
jby0909 <jb6377@naver.com>
2023-07-03 15:04:53 +0900

    test

---
+++ test.py
@@ -0,0 +1,30 @@
+####import math
+####print(math.pi)
+####print(math.sqrt(4.0))
+####print(math.sqrt(2.0))
+##
+##import math as mt
+##from math import pi
+##from math import sqrt
+##from math import fabs
+##from math import log10
+####from math import pi, sqrt
+####from math import *
+##
+##print(sqrt(4.0))
+##print(mt.sqrt(4.0))
+##print(mt.sqrt(2.0))
+##
+##
+##
+##print(sqrt(4.0))
+##print(pi)
+##
+##print(fabs(-20))
+##print(log10(1000))
+##
+
+
+import urllib.request as r
+response = r.urlopen('http://www.google.co.kr')
+print(response.status)
